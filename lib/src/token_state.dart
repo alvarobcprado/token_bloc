9c00a010e063fa98f08b3cd0985c4a691bf05175
Álvaro Prado <alvaroprado@tokenlab.com.br>
2023-05-26 16:00:39 -0400

    first commit

---
+++ lib/src/token_state.dart
@@ -0,0 +1,108 @@
+part of './token_state_manager.dart';
+
+/// The base class for all [TokenSubject]s.
+///
+/// Holds the subject and exposes only its [stream] getter and [close] method.
+abstract class TokenSubject<T, S extends Subject<T>> extends StreamView<T> {
+  TokenSubject(
+    this._subject,
+  ) : super(_subject.stream);
+
+  final S _subject;
+
+  Stream<T> get stream => _subject.stream;
+
+  void close() {
+    _subject.close();
+  }
+}
+
+/// A [TokenSubject] that holds a [PublishSubject].
+///
+/// It is used to dispatch actions with the [call] method.
+///
+/// Example:
+/// ```dart
+/// final action = TokenAction<int>();
+/// action(1);
+/// ```
+class TokenAction<T> extends TokenSubject<T, PublishSubject<T>> {
+  TokenAction({
+    void Function()? onListen,
+    void Function()? onCancel,
+    bool sync = false,
+  }) : super(
+          PublishSubject<T>(
+            onListen: onListen,
+            onCancel: onCancel,
+            sync: sync,
+          ),
+        );
+
+  void call(T value) {
+    _subject.add(value);
+  }
+}
+
+/// A [TokenAction] that holds [PublishSubject] with no value.
+///
+/// It is used to dispatch actions with the [call] method without passing a
+/// value.
+///
+/// Example:
+/// ```dart
+/// final action = TokenVoidAction();
+/// action();
+/// ```
+class TokenVoidAction extends TokenAction<void> {
+  TokenVoidAction({
+    super.onListen,
+    super.onCancel,
+    super.sync = false,
+  });
+
+  @override
+  void call([void value]) {
+    _subject.add(null);
+  }
+}
+
+/// A [TokenSubject] that holds a [BehaviorSubject].
+///
+/// It is used to hold the state of the view. It can be seeded with a initial
+/// value.
+///
+/// This value can be get and updated with the [valueOf] [valueOrNullOf] and
+/// [updateStateOf] methods that can be accessed only through the
+/// [TokenStateManager] classes.
+class TokenState<T> extends TokenSubject<T, BehaviorSubject<T>> {
+  TokenState({
+    void Function()? onListen,
+    void Function()? onCancel,
+    bool sync = false,
+  }) : super(
+          BehaviorSubject<T>(
+            onListen: onListen,
+            onCancel: onCancel,
+            sync: sync,
+          ),
+        );
+
+  TokenState.seeded(
+    T state, {
+    void Function()? onListen,
+    void Function()? onCancel,
+    bool sync = false,
+  }) : super(
+          BehaviorSubject<T>.seeded(
+            state,
+            onListen: onListen,
+            onCancel: onCancel,
+            sync: sync,
+          ),
+        );
+
+  T get _value => _subject.value;
+  T? get _valueOrNull => _subject.valueOrNull;
+  void _add(T value) => _subject.add(value);
+}
---
+++ lib/src/token_state_manager.dart
@@ -0,0 +1,177 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:rxdart/rxdart.dart';
+
+part './token_state.dart';
+
+extension _TokenStateManagerExtension on TokenState {
+  T valueOf<T>(TokenState<T> state) {
+    return state._value;
+  }
+
+  T? valueOrNullOf<T>(TokenState<T> state) {
+    return state._valueOrNull;
+  }
+
+  void updateStateOf<T>(TokenState<T> state, T newValue) {
+    state._add(newValue);
+  }
+}
+
+/// A [TokenStateManager] is a class that holds a list of [TokenSubject]s that
+/// can be used to manage the state of the view.
+///
+/// It must be extended and the [subjects] must be implemented with the list of
+/// [TokenSubject]s that will be used to manage the state of the view.
+///
+/// Example of usage:
+/// ```dart
+/// class ComboSuggestionBloc extends TokenStateManager {
+///   ComboSuggestionBloc({
+///     required this.getSnackByIdUC,
+///     required this.addSnackToCartUC,
+///     required String snackId,
+///     required String comboSuggestionId,
+///   }) {
+///     on(
+///       _fetchOptions(snackId, comboSuggestionId),
+///       onData: (value) => updateStateOf(onNewStateSubject, value),
+///     );
+
+///     on(
+///       onAddSnackSubject,
+///       onData: _addSnack,
+///     );
+///   }
+
+///   final GetSnackBarItemByIdUC getSnackByIdUC;
+///   final AddSnackToCartUC addSnackToCartUC;
+
+///   final onNewStateSubject = TokenState<ComboSuggestionState>();
+///   final onNewActionSubject = TokenAction<ComboSuggestionAction>();
+///   final onAddSnackSubject = TokenAction<Snack>();
+
+///   @override
+///   List<TokenSubject> get subjects => [
+///         onNewStateSubject,
+///         onNewActionSubject,
+///         onAddSnackSubject,
+///       ];
+
+///   Stream<ComboSuggestionState> _fetchOptions(
+///     String snackId,
+///     String comboSuggestionId,
+///   ) async* {
+///     final values = await Future.wait([
+///       getSnackByIdUC(
+///         GetSnackBarItemByIdUCParams(
+///           snackBarItemId: snackId,
+///         ),
+///       ),
+///       getSnackByIdUC(
+///         GetSnackBarItemByIdUCParams(
+///           snackBarItemId: comboSuggestionId,
+///         ),
+///       )
+///     ]);
+
+///     final snack = values[0] as Snack;
+///     final comboSnack = values[1] as ComboSnack;
+
+///     yield Success(
+///       snack: snack,
+///       comboSnack: comboSnack,
+///     );
+///   }
+
+///   Future<void> _addSnack(Snack snack) async {
+///     await addSnackToCartUC(
+///       AddSnackToCartUCParams(
+///         snackPick: SnackPick(
+///           cartUniqueId: getNewUniqueId(),
+///           snackId: snack.id,
+///         ),
+///       ),
+///     );
+///     onNewActionSubject(
+///       CloseDialogAfterAddSnack(
+///         snack: snack,
+///       ),
+///     );
+///   }
+/// }
+/// ```
+abstract class TokenStateManager {
+  final CompositeSubscription _subscriptions = CompositeSubscription();
+
+  /// The list of [TokenSubject]s that will be used to manage the state of the
+  /// view. It must be implemented.
+  List<TokenSubject> get subjects;
+
+  /// Listens to a [Stream] and adds the events to the [_subscriptions] list.
+  ///
+  /// It is a wrapper for the [Stream.listen] method.
+  void on<T>(
+    Stream<T> stream, {
+    void Function(T)? onData,
+    Function? onError,
+    void Function()? onDone,
+    bool? cancelOnError,
+  }) {
+    stream
+        .listen(
+          onData,
+          onError: onError,
+          onDone: onDone,
+          cancelOnError: cancelOnError,
+        )
+        .addTo(_subscriptions);
+  }
+
+  void onVoid(
+    Stream<void> stream, {
+    void Function()? onData,
+    Function? onError,
+    void Function()? onDone,
+    bool? cancelOnError,
+  }) {
+    on<void>(
+      stream,
+      onData: (_) => onData?.call(),
+      onError: onError,
+      onDone: onDone,
+      cancelOnError: cancelOnError,
+    );
+  }
+
+  /// Returns the value of a [TokenState].
+  ///
+  /// It is a wrapper for the [BehaviorSubject.value] getter.
+  T valueOf<T>(TokenState<T> state) {
+    return state.valueOf(state);
+  }
+
+  /// Returns the value of a [TokenState] or null if it has no value.
+  ///
+  /// It is a wrapper for the [BehaviorSubject.valueOrNull] getter.
+  T? valueOrNullOf<T>(TokenState<T> state) {
+    return state.valueOrNullOf(state);
+  }
+
+  /// Updates the value of a [TokenState].
+  ///
+  /// It is a wrapper for the [BehaviorSubject.add] method.
+  void updateStateOf<T, S extends T>(TokenState<T> state, S newValue) {
+    state._add(newValue);
+  }
+
+  /// Closes all the [TokenSubject]s.
+  @mustCallSuper
+  void dispose() {
+    for (final subject in subjects) {
+      subject.close();
+    }
+    _subscriptions.dispose();
+  }
+}
